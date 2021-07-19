f9e6e61141a46a88451c592d13ffe2ed07d13da4
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2021-07-19 16:02:10 -0500

    Update BroadcastCommand.php

--- src/JonyGamesYT9/SimpleBroadcast/Commands/BroadcastCommand.php
+++ src/JonyGamesYT9/SimpleBroadcast/Commands/BroadcastCommand.php
@@ -2,20 +2,26 @@
 
 namespace JonyGamesYT9\SimpleBroadcast\Commands;
 
+use JonyGamesYT9\SimpleBroadcast\SimpleBroadcast;
 use pocketmine\command\Command as Base;
 use pocketmine\command\CommandSender as Sender;
+use pocketmine\command\PluginIdentifiableCommand;
 
 /**
  * Class BroadcastCommand 
  * @package JonyGamesYT9\SimpleBroadcast\Commands
  */
-class BroadcastCommand extends Base {
+class BroadcastCommand extends Base implements PluginIdentifiableCommand {
   
+  /** @var SimpleBroadcast $plugin */
+  private $plugin;
+
   /**
    * BroadcastCommand Constructor.
    */
-  public function __construct()
+  public function __construct(SimpleBroadcast $plugin)
   {
+    $this->plugin = $plugin;
     parent::__construct("broadcast", "Broadcast System By: JonyGamesYT9", null, []);
   }
   
@@ -31,5 +37,13 @@ class BroadcastCommand extends Base {
     $sender->sendMessage("§7Author: §6JonyGamesYT9");
     $sender->sendMessage("§7Twitter: §6@JonySeGur");
     $sender->sendMessage("§7Github: §6JonyGamesYT9");
+  }
+
+  /**
+    * @return SimpleBroadcast
+    */
+  public function getPlugin(): SimpleBroadcast
+  {
+    return $this->plugin;
   }
 }
