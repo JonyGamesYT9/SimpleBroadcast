54618fed7d770d9dda9f288eab8a2cce4826bf14
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2021-12-01 20:44:09 -0600

    update BroadcastScheduler.php

--- src/JonyGamesYT9/SimpleBroadcast/Scheduler/BroadcastScheduler.php
+++ src/JonyGamesYT9/SimpleBroadcast/Scheduler/BroadcastScheduler.php
@@ -5,7 +5,7 @@ namespace JonyGamesYT9\SimpleBroadcast\Scheduler;
 use JonyGamesYT9\SimpleBroadcast\Provider\YamlProvider;
 use JonyGamesYT9\SimpleBroadcast\SimpleBroadcast;
 use pocketmine\Server;
-use pocketmine\Player;
+use pocketmine\player\Player;
 use function count;
 use function str_replace;
 use function array_rand;
